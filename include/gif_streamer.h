6e632482fe6d2caa4ce7ba19a1b5b4e9b02e9688
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2020-02-18 12:27:34 +0100

    Add a method to add pictures to GIF, a test main method is provided to buil a GIF from a sequence of BMP file

--- include/gif_streamer.h
+++ include/gif_streamer.h
@@ -21,7 +21,14 @@
 #include "gif_header.h"
 #include "gif_logical_screen.h"
 #include "gif_trailer.h"
+#include "gif_image.h"
+#include "gif_graphic_control_extension.h"
+#include "my_bmp.h"
+#include "my_color_alpha.h"
 #include <ostream>
+#include <string>
+#include <set>
+#include <map>
 
 namespace gif_streamer
 {
@@ -38,9 +45,32 @@ namespace gif_streamer
         inline
         ~gif_streamer();
 
+        inline
+        void send_bmp(std::string p_file_name, unsigned int p_delay_ms = 0);
+
+        inline
+        void send_trailer();
       private:
 
+        static inline
+        unsigned int convert_to_gif_color( const lib_bmp::my_color_alpha & p_bmp_color
+                                         , const std::map<lib_bmp::my_color_alpha,unsigned int> & p_conversion_table
+                                         );
+
         std::ostream & m_stream;
+
+        lib_gif::gif_logical_screen m_logical_screen;
+
+        bool m_first_picture;
+
+        /**
+         * Indicate if GIF trailer has been sent or not
+         */
+        bool m_trailer_streamed;
+
+        std::set<lib_bmp::my_color_alpha> m_global_palette;
+
+        std::map<lib_bmp::my_color_alpha,unsigned int> m_global_conversion_colors;
     };
 
     //-------------------------------------------------------------------------
@@ -49,9 +79,10 @@ namespace gif_streamer
                               , unsigned int p_height
                               )
     : m_stream(p_stream)
+    , m_logical_screen(p_width, p_height)
+    , m_first_picture(true)
+    , m_trailer_streamed(false)
     {
-        lib_gif::gif_header l_header;
-        l_header.write(p_stream);
         if(p_width >= 65536)
         {
             throw quicky_exception::quicky_logic_exception("Width value is greater than 65535 : " + std::to_string(p_width), __LINE__, __FILE__);
@@ -60,15 +91,161 @@ namespace gif_streamer
         {
             throw quicky_exception::quicky_logic_exception("Width value is greater than 65535 : " + std::to_string(p_width), __LINE__, __FILE__);
         }
-        lib_gif::gif_logical_screen l_logical_screen(p_width, p_height);
-        l_logical_screen.write(p_stream);
+        lib_gif::gif_header l_header;
+        l_header.write(p_stream);
     }
 
     //-------------------------------------------------------------------------
     gif_streamer::~gif_streamer()
     {
+        if(!m_trailer_streamed)
+        {
+            lib_gif::gif_trailer l_trailer;
+            l_trailer.write(m_stream);
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    gif_streamer::send_bmp(std::string p_file_name, unsigned int p_delay_ms)
+    {
+        // Check if file exist
+        std::ifstream l_file;
+        l_file.open(p_file_name.c_str(),std::ifstream::binary);
+        if(!l_file.is_open())
+        {
+            throw quicky_exception::quicky_runtime_exception(R"(Unable to read from file ")" + p_file_name + R"(")", __LINE__, __FILE__);
+        }
+        l_file.close();
+
+        lib_bmp::my_bmp l_bmp(p_file_name);
+
+        // Check picture resolution
+        if(l_bmp.get_width() != m_logical_screen.get_width() || l_bmp.get_height() != m_logical_screen.get_height())
+        {
+            throw quicky_exception::quicky_logic_exception("Image size ("
+                                                          + std::to_string(l_bmp.get_width()) + "*" + std::to_string(l_bmp.get_height())
+                                                          + ") doesn't correspond to GIF size ("
+                                                          + std::to_string(m_logical_screen.get_width()) + "*" + std::to_string(m_logical_screen.get_height())
+                                                          + ")", __LINE__, __FILE__
+                                                          );
+        }
+
+        // Check number of bits per pixel
+        if(8 != l_bmp.get_nb_bits_per_pixel())
+        {
+            throw quicky_exception::quicky_logic_exception("Only BMP files with 8 bits per pixel are supported : " + std::to_string(l_bmp.get_nb_bits_per_pixel()), __LINE__, __FILE__);
+        }
+
+        std::set<lib_bmp::my_color_alpha> l_local_palette;
+        std::set<lib_bmp::my_color_alpha> & l_palette = m_first_picture ? m_global_palette : l_local_palette;
+
+        // Fill the Palette
+        uint32_t l_palette_size = l_bmp.get_palette().get_size();
+        for(unsigned int l_index = 0 ; l_index < l_palette_size ; ++l_index)
+        {
+            l_palette.insert(l_bmp.get_palette().get_color(l_index));
+        }
+
+        if(l_palette.size() > 256)
+        {
+            throw quicky_exception::quicky_logic_exception(R"(Palette size from file ")" + p_file_name + R"(" is greater than 256 : )" + std::to_string(l_palette.size()),__LINE__,__FILE__);
+        }
+
+        // In case of non first picture, check if all colors are part of global palette
+        bool l_need_local_palette = false;
+        if(!m_first_picture)
+        {
+            // Check if all colors of picture's palette belong to global palette
+            for(unsigned int l_index = 0 ; l_index < l_palette_size && !l_need_local_palette; ++l_index)
+            {
+                l_need_local_palette = m_global_palette.end() == m_global_palette.find(l_bmp.get_palette().get_color(l_index));
+            }
+        }
+
+        std::map<lib_bmp::my_color_alpha,unsigned int> l_local_conversion_colors;
+        std::map<lib_bmp::my_color_alpha,unsigned int> & l_conversion_colors = (m_first_picture || !l_need_local_palette) ? m_global_conversion_colors : l_local_conversion_colors;
+
+        // Create conversion color to index structure
+        if(m_first_picture || l_need_local_palette)
+        {
+            unsigned int l_index = 0;
+            for(auto l_color_iter : l_palette)
+            {
+                l_conversion_colors.insert(std::map<lib_bmp::my_color_alpha,unsigned int>::value_type(l_color_iter,l_index));
+                ++l_index;
+            }
+        }
+
+        lib_gif::gif_image l_picture{(uint16_t)l_bmp.get_width(), (uint16_t)l_bmp.get_height(), 0, 0, l_need_local_palette, false, false, l_palette_size};
+        if(l_need_local_palette)
+        {
+            // Fill local palette
+            unsigned int l_index = 0;
+            for(auto l_color_iter : l_palette)
+            {
+                lib_gif::gif_color l_gif_color(l_color_iter.get_red(),l_color_iter.get_green(),l_color_iter.get_blue());
+                l_picture.set_color_in_color_table(l_index, l_gif_color);
+                ++l_index;
+            }
+        }
+        else if(m_first_picture)
+        {
+            // Fill global palette
+            unsigned int l_index = 0;
+            for(auto l_color_iter : l_palette)
+            {
+                lib_gif::gif_color l_gif_color(l_color_iter.get_red(),l_color_iter.get_green(),l_color_iter.get_blue());
+                m_logical_screen.set_color_in_global_color_table(l_index, l_gif_color);
+                ++l_index;
+            }
+
+            m_logical_screen.write(m_stream);
+            m_first_picture = false;
+        }
+
+        if(p_delay_ms)
+        {
+            lib_gif::gif_graphic_control_extension l_delay_extension(p_delay_ms, false, 0);
+            l_delay_extension.write(m_stream);
+        }
+
+        // Create gif picture
+        for(unsigned int l_y = 0; l_y < l_bmp.get_height(); ++ l_y)
+        {
+            for(unsigned int l_x = 0; l_x < l_bmp.get_width(); ++l_x)
+            {
+                l_picture.set_color_index(l_x, l_y, convert_to_gif_color(l_bmp.get_pixel_color(l_x, l_y), l_conversion_colors));
+            }
+        }
+        l_picture.write(m_stream);
+
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    gif_streamer::send_trailer()
+    {
         lib_gif::gif_trailer l_trailer;
         l_trailer.write(m_stream);
+        m_trailer_streamed = true;
+    }
+
+    //-------------------------------------------------------------------------
+    unsigned int
+    gif_streamer::convert_to_gif_color( const lib_bmp::my_color_alpha & p_bmp_color
+                                      , const std::map<lib_bmp::my_color_alpha, unsigned int> & p_conversion_table
+                                      )
+    {
+        auto l_color_iter = p_conversion_table.find(p_bmp_color);
+        if(p_conversion_table.end() != l_color_iter)
+        {
+            return l_color_iter->second;
+        }
+        std::stringstream l_color_stream;
+        l_color_stream << p_bmp_color;
+        throw quicky_exception::quicky_logic_exception("BMP color \"" + l_color_stream.str() + " not found insigned GIF palette",__LINE__,__FILE__);
+
     }
 
 }
--- src/main_gif_streamer.cpp
+++ src/main_gif_streamer.cpp
@@ -15,26 +15,101 @@
       along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 
-#include "quicky_exception.h"
-#include "gif_streamer.h"
-#include <iostream>
 
 #ifdef GIF_STREAMER_SELF_TEST
+#include "quicky_exception.h"
+#include "gif_streamer.h"
+#include "parameter_manager.h"
+#include <iostream>
 
 int main(int p_argc, char ** p_argv)
 {
     try
     {
+        // Defining application command line parameters
+        parameter_manager::parameter_manager l_param_manager("gif_streamer.exe","--",1);
+        parameter_manager::parameter_if l_name__pattern_parameter("name-pattern", false);
+        l_param_manager.add(l_name__pattern_parameter);
+        parameter_manager::parameter_if l_delay_ms_parameter("delay-ms",true);
+        l_param_manager.add(l_delay_ms_parameter);
+        parameter_manager::parameter_if l_start_index_parameter("start-index",true);
+        l_param_manager.add(l_start_index_parameter);
+        parameter_manager::parameter_if l_nb_files_parameter("nb-files",true);
+        l_param_manager.add(l_nb_files_parameter);
+
+        // Treating parameters
+        l_param_manager.treat_parameters(p_argc, p_argv);
+
+        std::string l_name_pattern = l_name__pattern_parameter.get_value<std::string>();
+        unsigned int l_delay = l_delay_ms_parameter.value_set() ? std::stoul(l_delay_ms_parameter.get_value<std::string>()) : 500;
+        unsigned int l_start_index = l_start_index_parameter.value_set() ? std::stoul(l_start_index_parameter.get_value<std::string>()) : 0;
+        unsigned int l_nb_files = l_nb_files_parameter.value_set() ? std::stoul(l_nb_files_parameter.get_value<std::string>()) : 0;
+
+        size_t l_pattern_pos = l_name_pattern.find('#');
+        if(std::string::npos == l_pattern_pos)
+        {
+            throw quicky_exception::quicky_logic_exception("Failed to find '#' character in file pattern \"" + l_name_pattern + "\"",__LINE__,__FILE__);
+        }
+
+        std::vector<std::string> l_file_list;
+
+        // Search files matching pattern
+        bool l_continu = true;
+        unsigned int l_file_index = l_start_index;
+        do
+        {
+            std::string l_file_name = l_name_pattern;
+            l_file_name.replace(l_pattern_pos,1,std::to_string(l_file_index));
+
+            // Check if file exist
+            std::ifstream l_file;
+            l_file.open(l_file_name.c_str(),std::ifstream::binary);
+            if(l_file.is_open())
+            {
+                l_file.close();
+                ++l_file_index;
+                l_file_list.emplace_back(l_file_name);
+            }
+            else
+            {
+                l_continu = false;
+            }
+            if(l_nb_files == l_file_index)
+            {
+                l_continu = false;
+            }
+        }
+        while(l_continu);
+
+        if(l_file_list.empty())
+        {
+            throw quicky_exception::quicky_logic_exception("No file found matching pattern \"" + l_name_pattern + "\"",__LINE__,__FILE__);
+        }
+
+        unsigned int l_width;
+        unsigned int l_height;
+        {
+            lib_bmp::my_bmp l_bmp(l_file_list.front());
+            l_width = l_bmp.get_width();
+            l_height = l_bmp.get_height();
+        }
+
         std::ofstream l_output_file;
-        std::string l_file_name{"stream.gif"};
-        l_output_file.open(l_file_name, std::ios::binary);
+        std::string l_gif_file_name{"stream.gif"};
+        l_output_file.open(l_gif_file_name, std::ios::binary);
         if(!l_output_file.is_open())
         {
-            throw quicky_exception::quicky_runtime_exception("Unable to create file \"" + l_file_name + "\"", __LINE__, __FILE__);
+            throw quicky_exception::quicky_runtime_exception("Unable to create file \"" + l_gif_file_name + "\"", __LINE__, __FILE__);
         }
 
         {
-            gif_streamer::gif_streamer l_stream(l_output_file, 640, 480);
+            gif_streamer::gif_streamer l_stream(l_output_file, l_width, l_height);
+            for(auto & l_file_iter:l_file_list)
+            {
+                std::cout << R"(Add file ")" + l_file_iter + R"(")" << std::endl;
+                l_stream.send_bmp(l_file_iter, l_delay);
+            }
+
         }
         l_output_file.close();
     }
